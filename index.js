f3e613d6c35805deba927ae57ca52615bf87545e
Logan Yu <yu.logan@gmail.com>
2022-06-26 00:58:25 -0400

    add working backend push

--- index.js
+++ index.js
@@ -37,6 +37,16 @@ console.log(`Password generator listening on ${port}`);
 const { OpenSeaStreamClient } = require('@opensea/stream-js');
 const { WebSocket } = require('ws');
 
+// EPNS
+const EpnsSDK = require("@epnsproject/backend-sdk-staging").default;
+const ethers = require('ethers');
+
+// the private key of the address which you used to create a channel
+const  CHANNEL_PK = '0x26c4c74285242b1552494ddc9d4038f3bb30e21831f95351b4ec0e884eb581f8';
+
+// Initialise the SDK
+const epnsSdk = new EpnsSDK(CHANNEL_PK);
+
 const openSeaClient = new OpenSeaStreamClient({
   token: 'c130c57a59cc497387c8c0883fb61306',
   connectOptions: {
@@ -48,23 +58,56 @@ function handleOpenSeaUpdates(collectionSlug) {
   console.log('handleOpenSeaUpdates')
   openSeaClient.connect();
 
-  openSeaClient.onItemMetadataUpdated(collectionSlug, (event) => {
+  openSeaClient.onItemMetadataUpdated(collectionSlug, async (event) => {
     console.log(event);
-    // handle event
+    await epnsSdk.sendNotification(
+      "0xD623dd87131062c6C91487f721F0B23CD472B758",
+      "Backend SDK Demo",
+      "This is demo for EPNS Backend SDK",
+      "Backend SDK Demo",
+      "This is demo for [d:EPNS Backend SDK]",
+      3,  
+      "<https://epns.io>"
+    );
   });
 
-  openSeaClient.onItemListed(collectionSlug, (event) => {
+  openSeaClient.onItemListed(collectionSlug, async (event) => {
     console.log(event);
-    // handle event
+    await epnsSdk.sendNotification(
+      "0x012eAA22F2286E615e582963c4b8F3F1a5646882",
+      "Backend SDK Demo",
+      "This is demo for EPNS Backend SDK",
+      "Backend SDK Demo",
+      "This is demo for [d:EPNS Backend SDK]",
+      3,  
+      "<https://epns.io>"
+    );
   });
 
-  openSeaClient.onItemSold(collectionSlug, (event) => {
+  openSeaClient.onItemSold(collectionSlug, async (event) => {
     console.log(event);
-    // handle event
+    await epnsSdk.sendNotification(
+      "0x012eAA22F2286E615e582963c4b8F3F1a5646882",
+      "Backend SDK Demo",
+      "This is demo for EPNS Backend SDK",
+      "Backend SDK Demo",
+      "This is demo for [d:EPNS Backend SDK]",
+      3,  
+      "<https://epns.io>"
+    );
   });
 
-  openSeaClient.onItemTransferred(collectionSlug, (event) => {
-    // handle event
+  openSeaClient.onItemTransferred(collectionSlug, async (event) => {
+    console.log(event);
+    await epnsSdk.sendNotification(
+      "0x012eAA22F2286E615e582963c4b8F3F1a5646882",
+      "Backend SDK Demo",
+      "This is demo for EPNS Backend SDK",
+      "Backend SDK Demo",
+      "This is demo for [d:EPNS Backend SDK]",
+      3,  
+      "<https://epns.io>"
+    );
   });
 }
 
